25148d6068704a928dbc080fa683aefc18fe38ae
Larry Smith Jr <mrlesmithjr@gmail.coml>
2016-05-19 11:31:12 -0400

    Fixed missing provisioning scripts and playbook

--- cleanup.sh
+++ cleanup.sh
@@ -1,1 +1,13 @@
-../cleanup.sh
+#!/bin/bash
+vagrant destroy -f
+for file in *.retry; do
+  if [[ -f $file ]]; then
+    rm $file
+  fi
+done
+if [ -d host_vars ]; then
+  rm -rf host_vars
+fi
+if [ -d .vagrant ]; then
+  rm -rf .vagrant
+fi
